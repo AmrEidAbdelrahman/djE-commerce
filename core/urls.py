3ebcb7d5eb157a929cdc2fe8bdd852b53d4320f1
AmrEidAbdelrahman <amreid248@gmail.com>
2022-01-05 23:13:02 +0200

    set base html and productview

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from . import views as core_views
 
 app_name = 'core'
 urlpatterns = [
-	path('home/', core_views.home, name='home-page'),
+	path('home/', core_views.HomeView.as_view(), name='home-page'),
+	path('product/<pk>', core_views.ProductDetailView.as_view(), name="product"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Item
+
+
 # Create your views here.
 
+class HomeView(ListView):
+	model = Item
+	template_name = 'core/home-page.html'
 
+class ProductDetailView(DetailView):
+	model = Item
+	template_name = 'core/product.html'
+
+'''
 def home(request):
+	context = {
+		'items': Item.objects.all(),
+	}
+	return render(request, 'core/home-page.html', context)
+'''
+
+def product(request):
 	context = {
 		'items': Item.objects.all(),
 	}
